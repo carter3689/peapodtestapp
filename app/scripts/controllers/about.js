e09540b944e4ef1e7c3a891b3ebb52a966689f76
Divisiondev84 <joel@division84.com>
2016-03-31 15:47:58 -0500

    initial commit :wq

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name peapodTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the peapodTestApp
+ */
+angular.module('peapodTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
